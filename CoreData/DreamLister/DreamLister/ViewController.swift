0aa20181087c3a7af2d74cab360884f3f69f0b4c
Bá Anh Nguyễn <baanh.nguyen@outlook.com>
2016-12-08 21:16:11 +0700

    DreamLister init

---
+++ CoreData/DreamLister/DreamLister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Bá Anh Nguyễn on 12/8/16.
+//  Copyright © 2016 Ba Anh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
