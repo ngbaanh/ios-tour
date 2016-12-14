d09b13d3b6bd59d9c11b34f92cfb732921481dac
Bá Anh Nguyễn <baanh.nguyen@outlook.com>
2016-12-14 16:41:09 +0700

    Rainy Shine App
    + init
    + pods
    + UI

---
+++ WebRequest-REST/RainyShinyCloudy/RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Bá Anh Nguyễn on 12/13/16.
+//  Copyright © 2016 Ba Anh Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
